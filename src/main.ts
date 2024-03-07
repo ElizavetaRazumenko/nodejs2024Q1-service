2da0a8bbb2ba0b40ebf829d7c03b5260e9ac4399
ElizavetaRazumenko <razumenko99@mail.ru>
2024-03-07 14:00:25 +0300

    feat: add swagger, dotenv, config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Home library')
+    .setDescription(
+      'Home Library Service! Users can create, read, update, delete data about Artists, Tracks and Albums, add them to Favorites in their own Home Library!',
+    )
+    .setVersion('1.0')
+    .addTag('music')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT);
 }
+
 bootstrap();
